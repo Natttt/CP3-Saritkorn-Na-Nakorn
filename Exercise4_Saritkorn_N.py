d72fc4232588e98d1611fec5abaea4fbb0515f42
Natttt <natsaritkorn@gmail.com>
2021-03-03 23:22:58 +0700

    Submit Excercise4

---
+++ Exercise4_Saritkorn_N.py
@@ -0,0 +1,16 @@
+Course1 = "Foundation English"
+Course2 = "General Business"
+Course3 = "Introduction to Computer Systems"
+Course4 = "Computer Programming"
+
+ScoreC1 = 100
+ScoreC2 = 75.83
+ScoreC3 = 80.94
+ScoreC4 = 99
+
+print("--- Your Score ---")
+print(Course1,":",ScoreC1)
+print(Course2,":", ScoreC2)
+print(Course3,":", ScoreC3)
+print(Course4,":", ScoreC4)
+
